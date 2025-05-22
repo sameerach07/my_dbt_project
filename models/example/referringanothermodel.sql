18df41076ef0130c34a8eebe02f77f25aa44ae9a
sameerach07@gmail.com <sameerach07@gmail.com>
2025-05-22 10:02:43 +0000

    committing

---
+++ models/example/referringanothermodel.sql
@@ -0,0 +1,11 @@
+{{ config(meterialized='table') }}
+
+with temp as (
+    select * from {{ ref('ConnectRawdata') }}
+)
+select a.* from temp a join {{ ref('usingsourcerepresentation')}} b 
+on a.client_id = b.client_id
+
+
+
+
